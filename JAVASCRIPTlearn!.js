6e45432d5e8a98fb1996578540883293b323798e
Jameeelaahmed <gamilas320@gmail.com>
2023-07-08 19:38:45 +0300

    add js dir

---
+++ JAVASCRIPTlearn!.js
@@ -0,0 +1,173 @@
+function here() {
+  // document.write("hello jaaaaaaam!");
+  // var name= prompt("Enw=ter your name ","enter here ");
+  // alert(name);
+  // var response=confirm("are you sure?");
+  // if(response==true)
+  // alert("thanks..");
+  // else
+  // alert("Goodbye");
+  // var x=prompt("enter first number");
+  // var y=prompt("enter second number");
+  // var result=parseInt(x)+parseInt(y);
+  // alert(result);
+  // for(let i=0;i<5;i++)
+  // {
+  //     alert(i);
+  // }
+}
+
+// * question 1 in HTML CODE
+
+// alert("welcome to my site");
+// let name = prompt("whats your name?");
+// document.write("welcome " + name + "!");
+
+// * question 2
+// function sum(n1, n2) {
+//   var z = parseInt(n1) + parseInt(n2);
+//   return z;
+// }
+// function calculateSum() {
+//   var x = prompt("Enter 1st number");
+//   var y = prompt("Enter 2nd number");
+//   var result = sum(x, y);
+//   alert("The sum is : " + result);
+// }
+
+// * question 3
+
+// function temprature(t) {
+//   if (t >= 30) {
+//     alert("HOT");
+//   } else {
+//     alert("cold");
+//   }
+// }
+// function printtemprature() {
+//   var x = prompt("enter temprature: ");
+//   temprature(x);
+// }
+
+// *question 4
+
+// function temp(temprature,actual)
+// {
+//   if (temprature >=25 && temprature<=30 && actual>=25 && actual>=30)
+//   {
+//     alert("Normal");
+//   }
+//   else if(temprature <25 && actual<25)
+//   {
+//     alert("Cold");
+//   }
+//   else if(temprature >30 && actual>30)
+//   {
+//     alert("Hot");
+//   }
+//   else 
+//   {
+//     alert("Ambigious");
+//   }
+// }
+
+
+
+// * question 5
+
+// function facullty(fac)
+// {
+//   var fac;
+//   if(fac =="FCI")
+//   alert("You’re eligible to Programing tracks");
+//   else if (fac=="Engineering")
+//   alert("You’re eligible to Network and Embedded tracks");
+//   else if (fac=="Commerce")
+//   alert("You’re eligible to ERP and Social media tracks");
+//   else 
+//   alert("You’re eligible to SW fundamentals track");
+// }
+
+// function collectFaculty()
+// {
+//   var x= prompt("Enter your faculty");
+//   facullty(x);
+// }
+// ! switch case 
+
+// function checkEligibility(faculty) {
+//   switch(faculty) {
+//     case 'FCI':
+//       return "You're eligible to Programming tracks";
+//     case 'Engineering':
+//       return "You're eligible to Network and Embedded tracks";
+//     case 'Commerce':
+//       return "You're eligible to ERP and Social media tracks";
+//     default:
+//       return "You're eligible to SW fundamentals track";
+//   }
+// }
+
+// * question 6
+
+// function odd(num1,num2)
+// { 
+//   for(let i=num1; i<=num2;++i)
+//   {
+//     if(i%2!=0)
+//     {
+//       alert(i);
+//     }
+//   }
+  
+// }
+
+// function print()
+// {
+//   var x,y;
+//   x=prompt("enter 1st number");
+//   y=prompt("enter 2nd number");
+//   odd(x,y);
+// }
+
+//* question 7
+
+
+//* question 8 
+
+// function check(name,year)
+// {
+//   if(t)
+//   alert("IS STRING");
+  
+//   if(!isNaN(year) && year<2010)
+//   alert("IS number ");
+// }
+
+// function vCheck()
+// {
+//   var x= prompt("Enter Your Name ");
+//   var y= prompt("Enter Your BirthYear ");
+//   check(x,y);
+// }
+
+// ! --------------
+// "strict mode"
+// var y;	
+// y=10; 
+// x = 5; 
+// console.log(x); 
+// console.log(y); 
+// var x; 
+
+// * BONUS 1
+// function h()
+// {
+//   for (let i=1;i<=6;i++)
+// {
+//   document.write("<h" + i + ">HI</h" + i+">");
+// }
+// }
+
+
+
